2b4f7557ed20522e2e4fff351d8dcbb2609a5345
warpersan <leumas.ecole@gmail.com>
2026-01-29 21:15:02 -0500

    PriorityIntersection

---
+++ include/Intersections/PriorityIntersection.h
@@ -0,0 +1,29 @@
+#ifndef INC_8INF259_TP1_PRIORITYINTERSECTION_H
+#define INC_8INF259_TP1_PRIORITYINTERSECTION_H
+#include <vector>
+
+#include "Intersection.h"
+#include "../Road.h"
+
+#define MINIMAL_DURATION 2
+#define MAXIMAL_DURATION 5
+
+/// Intersection that uses road density to prioritize directions
+class PriorityIntersection : public Intersection
+{
+	/// Every road processed by this intersection
+	std::vector<Road*> roads;
+	
+	/// Directions allowed to cross
+	Direction allowedDirections;
+	
+	/// Counter of the current duration
+	int durationCounter;
+public:
+	explicit PriorityIntersection(const std::vector<Road*>& roads);
+
+	void process() override;
+};
+
+
+#endif //INC_8INF259_TP1_PRIORITYINTERSECTION_H
---
+++ src/Intersections/PriorityIntersection.cpp
@@ -0,0 +1,66 @@
+#include "../../include/Intersections/PriorityIntersection.h"
+
+Direction getDensestDirection(const std::vector<Road*>& roads)
+{
+	Direction densestDirection = NONE;
+	std::size_t highestDensity = 0;
+	
+	for (const auto road : roads)
+	{
+		const auto density = road->count();
+		
+		if (density <= highestDensity)
+			continue;
+		
+		densestDirection = road->getDirection();
+		highestDensity = density;
+	}
+	
+	return densestDirection;
+}
+
+PriorityIntersection::PriorityIntersection(const std::vector<Road*>& roads) : Intersection()
+{
+	this->roads = roads;
+	this->allowedDirections = NONE;
+	this->durationCounter = 0;
+}
+
+void PriorityIntersection::process()
+{
+	if (this->durationCounter >= MAXIMAL_DURATION)
+	{
+		const auto currentDensestDirection = getDensestDirection(this->roads);
+		
+		this->allowedDirections = currentDensestDirection;
+		this->durationCounter = 0;
+	}
+	else if (this->durationCounter >= MINIMAL_DURATION)
+	{
+		const auto currentDensestDirection = getDensestDirection(this->roads);
+		
+		if (currentDensestDirection != this->allowedDirections)
+		{
+			this->allowedDirections = currentDensestDirection;
+			this->durationCounter = 0;
+		}
+	}
+	else if (this->allowedDirections == NONE)
+	{
+		const auto currentDensestDirection = getDensestDirection(this->roads);
+		
+		this->allowedDirections = currentDensestDirection;
+		this->durationCounter = 0;
+	}
+
+	for (const auto road : this->roads)
+	{
+		// ReSharper disable once CppTooWideScopeInitStatement
+		const auto direction = road->getDirection();
+		
+		if (direction & this->allowedDirections)
+			road->process();
+	}
+	
+	this->durationCounter++;
+}
