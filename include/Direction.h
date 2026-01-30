9140d413d6f1e1a7b537495d9c4dce5e8a0cf6ee
warpersan <leumas.ecole@gmail.com>
2026-01-29 22:30:04 -0500

    Removed unnecessary directions

--- include/Direction.h
+++ include/Direction.h
@@ -9,12 +9,7 @@ enum Direction : unsigned char
 	NORTH = 1 << 0,
 	SOUTH = 1 << 1,
 	EAST = 1 << 2,
-	WEST = 1 << 3,
-
-	NORTH_WEST = NORTH | WEST,
-	NORTH_EAST = SOUTH | EAST,
-	SOUTH_EAST = WEST | EAST,
-	SOUTH_WEST = NORTH | WEST
+	WEST = 1 << 3
 };
 
 /// Gets the opposite direction
