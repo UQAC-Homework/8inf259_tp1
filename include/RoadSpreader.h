2bd0e1918a3157b525d4dcd9be4f2a39dba52a43
warpersan <leumas.ecole@gmail.com>
2026-02-05 10:23:36 -0500

    Moved spreaders to `RoadSpreader`

---
+++ include/RoadSpreader.h
@@ -0,0 +1,81 @@
+#ifndef INC_8INF259_TP1_ROADSPREADER_H
+#define INC_8INF259_TP1_ROADSPREADER_H
+#include <memory>
+
+#include "Road.h"
+#include "Factories/VehicleFactory.h"
+
+inline void spreadEvenly(const std::vector<std::shared_ptr<Road>>& roads, const size_t totalCount)
+{
+	const size_t countPerRoad = totalCount / roads.size();
+
+	for (const auto& road : roads)
+	{
+		for (size_t i = 0; i < countPerRoad; i++)
+			road->addVehicle(VehicleFactory::create(road->getDirection()));
+	}
+
+	size_t remaining = totalCount - countPerRoad * roads.size();
+
+	for (const auto& road : roads)
+	{
+		if (remaining == 0)
+			break;
+
+		road->addVehicle(VehicleFactory::create(road->getDirection()));
+		remaining--;
+	}
+}
+
+inline void spreadOnDirections(const std::vector<std::shared_ptr<Road>>& roads, const Direction directions, const size_t totalCount)
+{
+	size_t affectedRoadCount = 0;
+
+	for (const auto& road : roads)
+	{
+		const auto direction = road->getDirection();
+
+		if (direction & directions)
+			affectedRoadCount++;
+	}
+
+	const size_t countPerRoad = totalCount / affectedRoadCount;
+
+	for (const auto& road : roads)
+	{
+		const auto direction = road->getDirection();
+
+		if (direction & directions)
+		{
+			for (size_t i = 0; i < countPerRoad; i++)
+				road->addVehicle(VehicleFactory::create(road->getDirection()));
+		}
+	}
+}
+
+inline void spreadUnbalanced(const std::vector<std::shared_ptr<Road>>& roads, const size_t northSouthCount, const size_t eastWestCount)
+{
+	for (int i = 0; i < northSouthCount; i++)
+	{
+		for (const auto& road : roads)
+		{
+			const auto direction = road->getDirection();
+			
+			if (direction & (NORTH | SOUTH))
+				road->addVehicle(VehicleFactory::create(road->getDirection()));
+		}
+	}
+	
+	for (int i = 0; i < eastWestCount; i++)
+	{
+		for (const auto& road : roads)
+		{
+			const auto direction = road->getDirection();
+			
+			if (direction & (EAST | WEST))
+				road->addVehicle(VehicleFactory::create(road->getDirection()));
+		}
+	}
+}
+
+#endif //INC_8INF259_TP1_ROADSPREADER_H
--- main.cpp
+++ main.cpp
@@ -2,85 +2,13 @@
 #include <fstream>
 #include <iostream>
 
+#include "include/RoadSpreader.h"
 #include "include/TrafficSystem.h"
 #include "include/Factories/VehicleFactory.h"
 #include "include/Intersections/FixedCycleIntersection.h"
 #include "include/Intersections/FourStopIntersection.h"
 #include "include/Intersections/Intersection.h"
 #include "include/Intersections/PriorityIntersection.h"
-
-void spreadEvenly(const std::vector<std::shared_ptr<Road>>& roads, const size_t totalCount)
-{
-	const size_t countPerRoad = totalCount / roads.size();
-
-	for (const auto& road : roads)
-	{
-		for (size_t i = 0; i < countPerRoad; i++)
-			road->addVehicle(VehicleFactory::create(road->getDirection()));
-	}
-
-	size_t remaining = totalCount - countPerRoad * roads.size();
-
-	for (const auto& road : roads)
-	{
-		if (remaining == 0)
-			break;
-
-		road->addVehicle(VehicleFactory::create(road->getDirection()));
-		remaining--;
-	}
-}
-
-void spreadOnDirections(const std::vector<std::shared_ptr<Road>>& roads, const Direction directions, const size_t totalCount)
-{
-	size_t affectedRoadCount = 0;
-
-	for (const auto& road : roads)
-	{
-		const auto direction = road->getDirection();
-
-		if (direction & directions)
-			affectedRoadCount++;
-	}
-
-	const size_t countPerRoad = totalCount / affectedRoadCount;
-
-	for (const auto& road : roads)
-	{
-		const auto direction = road->getDirection();
-
-		if (direction & directions)
-		{
-			for (size_t i = 0; i < countPerRoad; i++)
-				road->addVehicle(VehicleFactory::create(road->getDirection()));
-		}
-	}
-}
-
-void spreadUnbalanced(const std::vector<std::shared_ptr<Road>>& roads, const size_t northSouthCount, const size_t eastWestCount)
-{
-	for (int i = 0; i < northSouthCount; i++)
-	{
-		for (const auto& road : roads)
-		{
-			const auto direction = road->getDirection();
-			
-			if (direction & (NORTH | SOUTH))
-				road->addVehicle(VehicleFactory::create(road->getDirection()));
-		}
-	}
-	
-	for (int i = 0; i < eastWestCount; i++)
-	{
-		for (const auto& road : roads)
-		{
-			const auto direction = road->getDirection();
-			
-			if (direction & (EAST | WEST))
-				road->addVehicle(VehicleFactory::create(road->getDirection()));
-		}
-	}
-}
 
 int main()
 {
@@ -100,10 +28,10 @@ int main()
 
 	// Choose one of the following scenarios:
 	// Scenario 1: Spread traffic evenly on all roads
-	spreadEvenly(roads, 120);
+	//spreadEvenly(roads, 120);
 
 	// Scenario 2: Spread traffic evenly on all roads in the given directions
-	//spreadOnDirections(roads, NORTH | SOUTH, 120);
+	spreadOnDirections(roads, NORTH | SOUTH, 120);
 
 	// Scenario 3: Unbalanced traffic (50 vehicles N-S, 10 vehicles E-W = 120 total)
 	//spreadUnbalanced(roads, 50, 10);
@@ -113,7 +41,7 @@ int main()
 	PriorityIntersection intersection{
 		"Intersection Principale",
 		roads,
-		2,
+		3,
 		5
 	};
 
